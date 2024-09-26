c4104b8aaaf9383807a8b956f52d2c1d2bb7e723
sofia <sofia@localhost.localdomain>
2024-09-26 13:56:00 -0600

    updated train

--- train.py
+++ train.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="data.yaml", epochs=3)  # train the model
+model.train(data="/home/sofia/Documents/telponet/alt_dataset/data.yaml", epochs=2000, patience=100)  # train the model
 
 model.export(format="openvino", imgsz=640)  # export to OpenVINO
